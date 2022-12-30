5029a611ed8d814f22555a4ab9833fd9b52f43ce
arkgrv <ag.novitskiy@icloud.com>
2022-12-30 02:47:13 +0100

    Added constants

---
+++ subnet/src/constants.rs
@@ -0,0 +1,15 @@
+/// Standard CIDR values:
+/// * `UNDEF_CIDR`: undefined CIDR
+/// * `INVAL_CIDR`: invalid CIDR
+pub const UNDEF_CIDR: u8 = 0xFA;
+/// Standard CIDR values:
+/// * `UNDEF_CIDR`: undefined CIDR
+/// * `INVAL_CIDR`: invalid CIDR
+pub const INVAL_CIDR: u8 = 0xFF;
+
+/// Minimum block value (hex)
+/// This is the minimum value of an IP octet
+pub const MIN_BLOCK: u8 = 0x00;
+/// Maximum block value (hex)
+/// This is the maximum value of an IP octed
+pub const MAX_BLOCK: u8 = 0xFF;
---
+++ subnet/src/types.rs
@@ -0,0 +1,11 @@
+/// Represents a single IP address
+pub struct IPAddress {
+    /// First byte of IP address
+    pub b0: u8,
+    /// Second byte of IP address
+    pub b1: u8,
+    /// Third byte of IP address
+    pub b2: u8,
+    /// Fourth byte of IP address
+    pub b3: u8,
+}
